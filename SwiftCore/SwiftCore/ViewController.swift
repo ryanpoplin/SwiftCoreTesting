916ce696b1f8ae39a6101e68fb6c661403d91f44
Byrdann Fox <mobileupstate@gmail.com>
2015-02-19 11:34:43 -0500

    Updates...

--- SwiftCore/SwiftCore/ViewController.swift
+++ SwiftCore/SwiftCore/ViewController.swift
@@ -8,10 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var corePickerObject: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+        corePickerObject.dataSource = self
+        corePickerObject.delegate = self
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,6 +25,36 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        
+        if pickerView == corePickerObject {
+            return 1
+        }
+        
+        return 0
+        
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        if pickerView == corePickerObject {
+            return 2
+        }
+        
+        return 0
+        
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        
+        var corePickerObjectList = [
+            "Classes and Objects",
+            "Variables and Constants",
+        ]
+        
+        return "\(corePickerObjectList[row])"
+        
+    }
 
 }
